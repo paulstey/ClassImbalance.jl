4edb713d9f18f48ce985fb4b8cc307edb21784b3
paulstey <paulstey@gmail.com>
2017-02-21 16:21:36 -0500

    minor updates

--- test/smote_example.jl
+++ test/smote_example.jl
@@ -12,13 +12,26 @@ X = hcat(X, y)
 smote_exs(X, 11)
 
 
-w1 = [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+w1 = [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 cases_needed(w1)
-perc_needed(w1)
+pct_needed(w1)
 
 
 
-X = randn(100, 10)
-y = vcat(zeros(90), ones(10))
-ub_smote(X, y)
+X = randn(150, 10);
+y = vcat(zeros(140), ones(10));
+X2, y2 = smote(X, y, 5);
+
+length(y2)
+countmap(y2)
+
+
+
+
+X = randn(150, 10);
+y = vcat(zeros(130), ones(20));
+X2, y2 = smote(X, y, 5)
+
+length(y2)
+countmap(y2)
