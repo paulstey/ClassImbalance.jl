28f8a8cb38f325eefbea4f92ca8c97ae13c7e63a
paulstey <paulstey@gmail.com>
2017-03-13 16:31:37 -0400

    minor updates

--- test/simdata.jl
+++ test/simdata.jl
@@ -1,6 +1,6 @@
 # SimData
 using Distributions
-using DataFrames 
+using DataFrames
 using GLM
 
 n = 500
@@ -13,11 +13,11 @@ noise_coefs = zeros(p - 5)
 pr = 1./(1 + exp(-η))                                     # inv-logit
 
 # simulate outcome variable
-y = map(π -> rand(Binomial(1, π)), pr)                    
+y = map(π -> rand(Binomial(1, π)), pr)
 mean(y)
 
 df = DataFrame(X)
-df[:y] = y 
+df[:y] = y
 
 # keep only a sub-sample of positive cases
 keep_prop = 0.05
@@ -28,3 +28,42 @@ df2 = df[keep_indcs, :]
 sum(df2[:y]).value/n
 
 glm(y ~ 1 + x2 + x3 + x4 + x5 + x6 + x7, df2, Binomial())
+
+
+
+
+# Simulation of class-imbalanced data requires data drawn from
+# multivariate distribution (here we use MV Normal).
+
+n = 100
+p = 10
+# X = hcat(ones(n), randn(n, p-1))
+
+Σ = [1.0 0.8 0.4 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+     0.8 1.0 0.6 0.2 0.0 0.0 0.0 0.0 0.0 0.0;
+     0.4 0.6 1.0 0.6 0.4 0.0 0.0 0.0 0.0 0.0;
+     0.0 0.2 0.6 1.0 0.8 0.0 0.0 0.0 0.0 0.0;
+     0.0 0.0 0.4 0.8 1.0 0.0 0.0 0.0 0.0 0.0;
+     0.0 0.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 0.0;
+     0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0;
+     0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0 0.0;
+     0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0;
+     0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0]
+
+mvn = MvNormal(ones(p), Σ)
+
+srand(round(Int, time()))
+
+X = rand(mvn, n)'
+cor(X)
+
+ϵ = rand(Normal(2, 0.5), n)                                  # gives 10% minority class,
+# ϵ = rand(Normal(-2.5, 0.5), n)                               # gives 5% minority class,
+# ϵ = rand(Normal(4, 0.5), n)                                  # gives 15% minority class,
+
+β = [-3, -4, 1, -5, -4, 0.0, 0.0, 0.0, 0.0, 0.0]
+η = X*β + ϵ                                                    # linear predictor w/ error
+pr = 1.0 ./ (1.0 + exp(-η))                                    # inv-logit
+
+# simulate outcome variable
+y = map(π -> rand(Binomial(1, π)), pr)
